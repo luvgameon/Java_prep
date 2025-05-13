5e3b013a21ce87c3bea0759f8c1361b910452ad3
Love Bhardwaj <love.bhardwaj@hcltech.com>
2025-05-13 11:09:56 +0530

    .j

--- function/question/PrimeNumber.java
+++ function/question/PrimeNumber.java
@@ -14,7 +14,7 @@ public class PrimeNumber {
         if(num <= 1){
             return false;
         }
-        for(int i = 2; i <= Math.sqrt(num); i++){
+        for(int i = 2; i*i <= num; i++){
             if(num % i == 0){
                 return false;
             }
